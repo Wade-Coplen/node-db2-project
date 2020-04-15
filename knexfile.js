77b5d4c16a1f509c08b3eba7198bcc9c9ce470f3
Wade-Coplen <wadecoplen@gmail.com>
2020-04-14 20:15:00 -0700

    4- mvp

--- knexfile.js
+++ knexfile.js
@@ -15,39 +15,39 @@ module.exports = {
       }
     },
   
-    // staging: {
-    //   client: 'postgresql',
-    //   connection: {
-    //     database: 'my_db',
-    //     user:     'username',
-    //     password: 'password'
-    //   },
-    //   pool: {
-    //     min: 2,
-    //     max: 10
-    //   },
-    //   migrations: {
-    //     tableName: 'knex_migrations'
-    //   }
-    // },
-    // production: {
-    //   useNullAsDefault: true,
-    //   client: 'pg',
-    //   connection: 
-    //     process.env.DATABASE_URL
-    //   ,
-    //   pool: {
-    //     min: 2,
-    //     max: 10
-    //   },
-    //   migrations: {
-    //     directory: "./data/migrations"
-    //   },
-    //   seeds: {
-    //     directory: "./data/seeds"
+    staging: {
+      client: 'postgresql',
+      connection: {
+        database: 'my_db',
+        user:     'username',
+        password: 'password'
+      },
+      pool: {
+        min: 2,
+        max: 10
+      },
+      migrations: {
+        tableName: 'knex_migrations'
+      }
+    },
+    production: {
+      useNullAsDefault: true,
+      client: 'pg',
+      connection: 
+        process.env.DATABASE_URL
+      ,
+      pool: {
+        min: 2,
+        max: 10
+      },
+      migrations: {
+        directory: "./data/migrations"
+      },
+      seeds: {
+        directory: "./data/seeds"
   
-    //   }
+      }
   
-    // }
+    }
   
   };
